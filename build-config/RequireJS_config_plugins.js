14377f39f61f9cd55ea60e44651269be3db4e091
Tarek Alahy <tarekalahy@MacBook-Pro-de-Tarek.local>
2016-07-07 18:52:36 +0200

    Completed documentation for the smil_iterator.js

---
+++ build-config/RequireJS_config_plugins.js
@@ -0,0 +1,11 @@
+//Do not modify this file, it is automatically generated.
+if(window._RJS_isBrowser){var p=[],c=require.config;require.config=function(e){c.apply(require,arguments);var r=e.packages;r&&(p=r),require.config=c,window._RJS_pluginsList=p.map(function(e){return e.name})}}
+require.config({packages:[
+{
+name: "readium_plugin_highlights",
+location: process._RJS_rootDir(1) + "/plugins/highlights",
+main: "main"
+},
+],
+
+});
---
+++ build-config/RequireJS_config_plugins_multiple-bundles.js
@@ -0,0 +1,10 @@
+//Do not modify this file, it is automatically generated.
+require.config({modules:[
+{
+name: "readium-plugin-highlights",
+create: true,
+include: ["readium_plugin_highlights"],
+exclude: ["readium-external-libs", "readium-cfi-js", "readium-shared-js"],
+insertRequire: ["readium_plugin_highlights"]
+},
+]});
